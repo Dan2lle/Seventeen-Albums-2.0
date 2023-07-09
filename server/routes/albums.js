9ac0141a29fa027abcef951d348cc50f0233c7c5
Danielle Chen <jt17@students.cs.ubc.ca>
2023-07-09 00:12:28 -0700

    ability to update an album

--- server/routes/albums.js
+++ server/routes/albums.js
@@ -1,40 +1,10 @@
+// referenced from: https://www.youtube.com/watch?v=vjf774RKrLc&t=2093s
+
 var express = require('express');
 var router = express.Router();
 var Album = require('../model/Album');
 
-let albums = [
-  {
-    id: "1",
-    album: "17 CARAT",
-    description: "1st mini album",
-    price: "29",
-    image: "https://upload.wikimedia.org/wikipedia/en/2/21/Seventeen-17_Carat_%28EP%29.jpg",
-  },
-  {
-    id: "2",
-    album: "BOYS BE",
-    description: "2nd mini album",
-    price: "27",
-    image: "https://upload.wikimedia.org/wikipedia/en/d/dc/Boys_Be_EP.png",
-  },
-  {
-    id: "3",
-    album: "LOVE&LETTER",
-    description: "1st full album",
-    price: "35",
-    image: "https://upload.wikimedia.org/wikipedia/en/d/de/Seventeen_-_Love_and_Letter.jpg"
-  },
-  {
-    id: "4",
-    album: "Going Seventeen",
-    description: "3rd mini album",
-    price: "39",
-    image: "https://upload.wikimedia.org/wikipedia/en/f/f5/Going_Seventeen_EP.jpg"
-  }
-]
-
 /* GET users listing. */
-/* referenced from: https://levelup.gitconnected.com/node-js-filtering-sorting-and-pagination-50ce6c90d0ad */
 router.get('/', async (req, res, next) => {
   try {
     const albumData = await Album.find({})
@@ -62,23 +32,26 @@ router.get('/:albumId', async (req, res, next) => {
   return res.status(200).send(foundAlbum);
 });
 
-router.put('/:albumId', function(req, res, next) {
-  let i = 0;
-  while (i < albums.length) {
-    if (albums[i].id === req.params.albumId) {
-      albums[i].album = req.body.album
-      albums[i].description = req.body.description
-      albums[i].price = req.body.price
-      albums[i].image = req.body.image
-      return res.status(200).send(albums[i]);
-    } else {
-      ++i
-    }
+router.put('/:albumId', async(req, res, next) => {
+  try {
+    const updatedAlbum = await Album.updateOne(
+      { _id: req.params.albumId },
+      { $set: 
+        { album: req.body.album,
+          description: req.body.description, 
+          title: req.body.title, 
+          price: req.body.price, 
+          released: req.body.released, 
+          image: req.body.image 
+        }
+      }
+    );
+    res.status(200).json(updatedAlbum)
+  } catch (err) {
+    res.json({ message: err })
   }
-  return res.status(404).send({ message: 'Album not found'})
 });
 
-/* referenced from: https://www.youtube.com/watch?v=vjf774RKrLc&list=WL&index=29 */
 router.post('/', async (req, res, next) => {
   if (!req.body.album) {
     return res.status(400).send({ message: 'Album must have a name!' })
@@ -110,3 +83,5 @@ router.delete('/:albumId', async (req, res, next) => {
 });
 
 module.exports = router;
+
+// able to load all, add one, delete one, update one, sort all
