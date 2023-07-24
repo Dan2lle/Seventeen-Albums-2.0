77450823026b3e59f88d78e45e6fb479c52b6ff8
Danielle Chen <jt17@students.cs.ubc.ca>
2023-07-23 17:47:55 -0700

    dynamic FE and BE url

--- client/src/apis/api.js
+++ client/src/apis/api.js
@@ -1,5 +1,23 @@
 import axios from "axios";
 
+export const getBaseUrl = () => {
+    let url;
+    switch(process.env.NODE_ENV) {
+      case 'production':
+        console.log('production')
+        url = "https://seventeen-albums-right-here.onrender.com";
+        break;
+      case 'development':
+        console.log('development')
+        url = "http://localhost:3002";
+        break;
+      default:
+        url = "https://seventeen-albums-right-here.onrender.com";
+    }
+    console.log(url)
+    return url;
+}
+
 export default axios.create({
-    baseURL: "https://seventeen-albums-right-here.onrender.com"
+    baseURL: getBaseUrl()
 })
